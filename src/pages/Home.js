17bef6707d781aeffe5063a0e3c729cf9f540cb0
Tunde James <tundejames04@gmail.com>
2021-05-27 13:00:28 +0100

    First commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import JumbotronContainer from "../containers/JumbotronContainer"
+import FooterContainer from "../containers/FooterContainer"
+
+function Home() {
+  return (
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
+  )
+}
+
+export default Home
